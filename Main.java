286d01e6b0561a300965130385bad64ad3bff155
jlbraun <137731436+jlbraun@users.noreply.github.com>
2023-06-26 21:48:57 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
         SignInPage signInPage = new SignInPage(primaryStage);
         Scene scene = new Scene(signInPage.getRoot(), 800, 600);
-        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        //scene.getStylesheets().add(getClass().getResource("C:\\Users\\LoNeZiLLa\\Documents\\ASU\\Classes\\Summer 2023\\CSE 360\\Class Project\\Phase 3\\prototype\\demo\\src\\main\\java\\prototype\\demo\\application.css").toExternalForm());
 
         primaryStage.setScene(scene);
         primaryStage.show();
