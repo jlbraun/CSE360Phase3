5ed69ea8f3b2f9c3ff6ba7dccb5438c138f78a5b
jlbraun <137731436+jlbraun@users.noreply.github.com>
2023-06-26 21:49:10 -0700

    Update PatientLoginPage.java

--- PatientLoginPage.java
+++ PatientLoginPage.java
@@ -22,26 +22,26 @@ public class PatientLoginPage {
     }
 
     private void createSignInPage() {
-        // Create the logo image view
-        Image logoImage = new Image("logo.jpg"); 
-        ImageView logoImageView = new ImageView(logoImage); 
-        logoImageView.setFitWidth(220);
-        logoImageView.setFitHeight(220);
-
-        Label titleLabel = new Label("Patient Login");
-        titleLabel.getStyleClass().add("title-label");
+//        // Create the logo image view
+//        Image logoImage = new Image("C:\\Users\\LoNeZiLLa\\Documents\\ASU\\Classes\\Summer 2023\\CSE 360\\Class Project\\Phase 3\\prototype\\demo\\src\\main\\java\\prototype\\demo\\logo.jpg");
+//        ImageView logoImageView = new ImageView(logoImage);
+//        logoImageView.setFitWidth(220);
+//        logoImageView.setFitHeight(220);
+//
+//        Label titleLabel = new Label("Patient Login");
+//        titleLabel.getStyleClass().add("title-label");
         
         // Create the text fields
         TextField usernameTextField = new TextField();
         usernameTextField.setPromptText("Username");
         usernameTextField.setMaxWidth(200);
-        usernameTextField.getStyleClass().add("username-text-field");
+        //usernameTextField.getStyleClass().add("username-text-field");
 
         // Create the buttons
         Button loginButton = new Button("Login");
         Button createAccountButton = new Button("Create Account");
-        loginButton.getStyleClass().add("dark-button");
-        createAccountButton.getStyleClass().add("light-button");
+        //loginButton.getStyleClass().add("dark-button");
+        //createAccountButton.getStyleClass().add("light-button");
 
         // Create an HBox for the buttons
         VBox buttonBox = new VBox(10);
@@ -54,7 +54,7 @@ public class PatientLoginPage {
         VBox signInBox = new VBox(10);
         signInBox.setAlignment(Pos.CENTER);
         signInBox.setSpacing(20);
-        signInBox.getChildren().addAll(titleLabel, usernameTextField, buttonBox);
+        signInBox.getChildren().addAll(usernameTextField, buttonBox);
 
         // Create the home button
         Button homeButton = new Button("Home");
@@ -64,6 +64,22 @@ public class PatientLoginPage {
             // Logic to navigate to home page
         	SignInPage signInPage = new SignInPage(primaryStage);
             primaryStage.setScene(new Scene(signInPage.getRoot(), 900, 600));
+            primaryStage.setResizable(false);
+            primaryStage.setFullScreen(false);
+        });
+        
+        loginButton.setOnAction(e -> {
+            // Handle staff login button click
+            PatientPortal patientPortal = new PatientPortal(primaryStage);
+            primaryStage.setScene(new Scene(patientPortal.getRoot(), 900, 600));
+            primaryStage.setResizable(false);
+            primaryStage.setFullScreen(false);
+        });
+        
+        createAccountButton.setOnAction(e -> {
+            // Handle staff login button click
+            CreatePatientAccount createPatientAccount = new CreatePatientAccount(primaryStage);
+            primaryStage.setScene(new Scene(createPatientAccount.getRoot(), 900, 600));
             primaryStage.setResizable(false);
             primaryStage.setFullScreen(false);
         });
@@ -79,12 +95,12 @@ public class PatientLoginPage {
         root.setHgap(50);
         root.setVgap(100);
         root.setPadding(new Insets(10));
-        root.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        //root.getStylesheets().add(getClass().getResource("C:\\Users\\LoNeZiLLa\\Documents\\ASU\\Classes\\Summer 2023\\CSE 360\\Class Project\\Phase 3\\prototype\\demo\\src\\main\\java\\prototype\\demo\\application.css").toExternalForm());
         root.add(topBox, 0, 0);
         root.add(signInBox, 1, 1);
         GridPane.setMargin(signInBox, new Insets(-50, 0, 0, 30));
-        root.add(logoImageView, 2, 0, 1, 3);
-        GridPane.setMargin(logoImageView, new Insets(-100, 0, 0, 0));
+        //root.add(logoImageView, 2, 0, 1, 3);
+        //GridPane.setMargin(logoImageView, new Insets(-100, 0, 0, 0));
     }
 
     public GridPane getRoot() {
