ac0f71bb1bc27847022dd73854ef67188d5f7da4
jlbraun <137731436+jlbraun@users.noreply.github.com>
2023-06-30 15:20:19 -0700

    Update CreatePatientAccount.java

--- CreatePatientAccount.java
+++ CreatePatientAccount.java
@@ -1,14 +1,16 @@
-package prototype.demo;
+package application;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,32 +26,55 @@ public class CreatePatientAccount {
     }
 
     private void createPatientAccount() {
-        // Create the buttons
+        
+        Button homeButton = new Button("Home");
+        homeButton.getStyleClass().add("dark-button");
+        homeButton.setOnAction(e -> {
+            // Handle home button click
+            // Logic to navigate to home page
+        	SignInPage signInPage = new SignInPage(primaryStage);
+            primaryStage.setScene(new Scene(signInPage.getRoot(), 900, 600));
+            primaryStage.setResizable(false);
+            primaryStage.setFullScreen(false);
+        });
+        
+        VBox topBox = new VBox(10);
+        topBox.setAlignment(Pos.TOP_LEFT);
+        topBox.setPadding(new Insets(50, 0, 0, 50));
+        topBox.getChildren().addAll(homeButton);
+    	
         Button createAccountButton = new Button("Create Account");
-        Button exitButton = new Button("Exit");
+        createAccountButton.getStyleClass().add("create-account-button");
+        
+        Label titleLabel = new Label("Create Patient \n     Account");
+        titleLabel.setWrapText(true);
+        titleLabel.getStyleClass().add("create-patient-label");
 
-        // Create Labels
-        Text windowLabel = new Text("Create New Patient Account");
-        Text usernameText = new Text("Enter Username: ");
-        Text accountCreatedText = new Text("Account Created!");
-
-        // Create username TextField
+        Label usernameLabel = new Label("Enter username: ");
         TextField usernameField = new TextField();
-        usernameField.setMaxWidth(300);
 
+        GridPane inputBox = new GridPane();
+        inputBox.setHgap(10); 
+        inputBox.setVgap(10); 
+        inputBox.setPadding(new Insets(10));
 
+        inputBox.add(usernameLabel, 0, 0); // Column 0, Row 0
+        inputBox.add(usernameField, 1, 0); // Column 1, Row 0
+        inputBox.add(createAccountButton, 1, 3);
+        GridPane.setMargin(createAccountButton, new Insets(20, 0, 0, -20));
+        
         // Add event handlers for the buttons
         createAccountButton.setOnAction(e -> {
             if (!usernameField.equals("")) {
-                File file;
-                // Handle button click
-                PatientFiles patientFiles = new PatientFiles();
-                try {
-                    file = patientFiles.createPatientInfoFile(usernameField.getText());
-                    boolean result = file.createNewFile();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+            	boolean bool;
+          		File patientFile = new File("C:\\Users\\jlbdv\\eclipse-workspace\\javafxtest\\src\\application\\PatientInfoFiles\\" + usernameField.getText() +  "_patientInfo.txt");
+          		File patientMessageFile = new File("C:\\Users\\jlbdv\\eclipse-workspace\\javafxtest\\src\\application\\PatientInfoFiles\\" + usernameField.getText() +  "_patientMessage.txt");
+          		try {
+    				bool = patientFile.createNewFile();
+    				bool = patientMessageFile.createNewFile();
+    			} catch (IOException e1) {
+    				e1.printStackTrace();
+    			}
                 PatientPortal patientPortal = new PatientPortal(primaryStage, usernameField.getText());
                 primaryStage.setScene(new Scene(patientPortal.getRoot(), 900, 600));
                 primaryStage.setResizable(false);
@@ -57,38 +82,22 @@ public class CreatePatientAccount {
             }
         });
         
-        exitButton.setOnAction(e -> {
-            // Handle patient login button click
-        	SignInPage signInPage = new SignInPage(primaryStage);
-            primaryStage.setScene(new Scene(signInPage.getRoot(), 900, 600));
-            primaryStage.setResizable(false);
-            primaryStage.setFullScreen(false);
-        });
-
-        VBox leftBox = new VBox(10);
-        leftBox.setAlignment(Pos.TOP_LEFT);
-        leftBox.getChildren().add(usernameText);
-
-        VBox centerBox = new VBox(10);
-        centerBox.setAlignment(Pos.CENTER);
-        centerBox.getChildren().addAll(windowLabel, usernameField, createAccountButton, exitButton);
-
-        // Create a VBox to hold the buttons
         root = new GridPane();
         root.setHgap(50);
         root.setVgap(100);
         root.setPadding(new Insets(10));
-        root.setAlignment(Pos.TOP_LEFT);
-        root.add(leftBox, 1, 1);
-        root.add(centerBox, 2, 1);
+        root.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        root.add(topBox, 0, 0);
+       
+        root.add(titleLabel, 1, 0);
+        root.add(inputBox, 1, 1);
+        GridPane.setMargin(titleLabel, new Insets(30, 0, 0, 60));
+        GridPane.setMargin(inputBox, new Insets(-50, 0, 0, 60));
 
-        VBox.setMargin(usernameText, new Insets(25, 0, 0, 0));
-//        root.setPadding(new Insets(20));
-        //root.getChildren().addAll(windowLabel, createAccountButton, exitButton);
-        //root.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        
+      
 
-        // Set spacing between the buttons
-        //VBox.setMargin(viewInfoButton, new Insets(10, 0, 0, 0));
+        
     }
 
     public GridPane getRoot() {
